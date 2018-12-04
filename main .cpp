ec06060fd18f4f1714a793d51fb87469b55124c8
Okin Grigoriy <okint34@yandex.ru>
2018-12-04 14:37:44 +0300

    updating Main File;
    add new func.

--- main .cpp
+++ main .cpp
@@ -30,14 +30,14 @@ struct sorting{
 
 // reading from files
 const std::vector<char> readFile(const std::string fileName) {
-	std::ifstream inputFile = std::ifstream(fileName);
-	std::vector<char> inputChars = std::vector<char>();
-	char currentCharacter = 0;
-	while (inputFile.get(currentCharacter)) {
-		inputChars.push_back(currentCharacter);
-	}
-	inputFile.close();
-	return inputChars;
+    std::ifstream inputFile = std::ifstream(fileName);
+    std::vector<char> inputChars = std::vector<char>();
+    char currentCharacter = 0;
+    while (inputFile.get(currentCharacter)) {
+        inputChars.push_back(currentCharacter);
+    }
+    inputFile.close();
+    return inputChars;
 }
 // writing from files
 void wriringOnFile(const std::vector<char>encodedText, const std::string fileName){
@@ -50,37 +50,36 @@ void wriringOnFile(const std::vector<char>encodedText, const std::string fileNam
 
 //encoding Func
 const std::vector<char> encodeData(const std::vector<char> decodedData) {
-	const std::size_t length = decodedData.size();
-	std::vector<char> encodedData = std::vector<char>();
-	
-	char lastChar = 0;
-	for(std::size_t i = 0; i < length; ++i) {
-		const char currentChar = decodedData[i];
-		const char encodedChar = currentChar - lastChar;
-		encodedData.push_back(encodedChar);
-		lastChar = currentChar;
-	}
-	return encodedData;
+    const std::size_t length = decodedData.size();
+    std::vector<char> encodedData = std::vector<char>();
+    
+    char lastChar = 0;
+    for(std::size_t i = 0; i < length; ++i) {
+        const char currentChar = decodedData[i];
+        const char encodedChar = currentChar - lastChar;
+        encodedData.push_back(encodedChar);
+        lastChar = currentChar;
+    }
+    return encodedData;
 }
 
 // decoding Func
 const std::vector<char> decodeData(const std::vector<char> encodedData) {
-	const std::size_t length = encodedData.size();
-	std::vector<char> decodedData = std::vector<char>();
-	
-	char lastChar = 0;
-	for(std::size_t i = 0; i < length; ++i) {
-		const char currentChar = encodedData[i];
-		const char decodedChar = currentChar + lastChar;
-		decodedData.push_back(decodedChar);
-		lastChar = decodedChar;
-	}
-	
-	return decodedData;
+    const std::size_t length = encodedData.size();
+    std::vector<char> decodedData = std::vector<char>();
+    
+    char lastChar = 0;
+    for(std::size_t i = 0; i < length; ++i) {
+        const char currentChar = encodedData[i];
+        const char decodedChar = currentChar + lastChar;
+        decodedData.push_back(decodedChar);
+        lastChar = decodedChar;
+    }
+    
+    return decodedData;
 }
 //add chars in array and counting them
-const std::map<char,int> counter(const std::string& s)
-{
+const std::map<char,int> counter(const std::string& s){
     std::map<char,int> charsInString;
     for (auto character : s) {
         charsInString[character]++;
@@ -88,8 +87,8 @@ const std::map<char,int> counter(const std::string& s)
     return charsInString;
 }
 
- std::list<Node *> creatyNodeList(const std::map<char,int> charsInString){
-
+std::list<Node *> creatyNodeList(const std::map<char,int> charsInString){
+    
     std::list<Node *> nodeList = std::list<Node *>();
     std::map<char,int>::iterator iter;
     for(auto iter=charsInString.begin(); iter!=charsInString.end(); iter++) {
@@ -102,14 +101,14 @@ const std::map<char,int> counter(const std::string& s)
 }
 
 Node *creatingTree( std::list<Node *> nodeList){
-
+    
     while (nodeList.size() != 1) {
         nodeList.sort(sorting());
         Node *sonOnLeft = nodeList.front();
-//        std::cout<<sonOnLeft->character << sonOnLeft->qty <<' '<<sonOnLeft->left << ' '<<sonOnLeft->right<<std::endl;
+        //        std::cout<<sonOnLeft->character << sonOnLeft->qty <<' '<<sonOnLeft->left << ' '<<sonOnLeft->right<<std::endl;
         nodeList.pop_front();
         Node *sonOnRight = nodeList.front();
-//        std::cout << sonOnRight->character << sonOnRight->qty << ' '<< sonOnRight->left << ' ' << sonOnRight->right<<std::endl;
+        //        std::cout << sonOnRight->character << sonOnRight->qty << ' '<< sonOnRight->left << ' ' << sonOnRight->right<<std::endl;
         nodeList.pop_front();
         
         Node *father = new Node(sonOnLeft, sonOnRight);
@@ -122,11 +121,11 @@ Node *creatingTree( std::list<Node *> nodeList){
 std::vector<bool> code;
 std::map<char,std::vector<bool>> table;
 /* table view
-m 1010
-g 11100
-t 0
-/n 11101
-*/
+ m 1010
+ g 11100
+ t 0
+ /n 11101
+ */
 void createTable(Node *root){
     
     if (root->left != NULL){
@@ -140,22 +139,22 @@ void createTable(Node *root){
     if (root->character){
         table[root->character] = code;
         /*
-        std::cout << "Character is " << root->character << ' ';
-        for (int i = 0 ; i<code.size(); i++) {
-            std::cout << code[i];
-        }
-        std::cout<< std::endl;
-        */
+         std::cout << "Character is " << root->character << ' ';
+         for (int i = 0 ; i<code.size(); i++) {
+         std::cout << code[i];
+         }
+         std::cout<< std::endl;
+         */
     }
     code.pop_back();
 }
 
 std::string printedTable(const std::string inputString){
-
+    
     std::string bitString;
     for (int i = 0 ; i<inputString.length(); i++) {
         std::vector<bool> tbl = table[inputString[i]];
-
+        
         for (int j = 0; j<tbl.size(); j++) {
             if (tbl[j]==0) {
                 bitString += "0";
@@ -198,28 +197,30 @@ const std::vector<char> bitWriting( std::string bitString){
 }
 
 int main(int argc, const char * argv[]) {
-	
+    
     const std::string inputPath = "/Users/kokos/Documents/Prog/SuperDifficultProgram/InputText.txt";
     const std::string outputPath ="/Users/kokos/Documents/Prog/SuperDifficultProgram/OutputText.txt";
-	const std::string inputFileName = inputPath;
+    const std::string inputFileName = inputPath;
     const std::string outputFileName = outputPath;
-	const std::vector<char> inputChars = readFile(inputFileName);
+    const std::vector<char> inputChars = readFile(inputFileName);
     std::string inputString = std::string(inputChars.begin(), inputChars.end());
     std::cout << inputString << std::endl;
-//    const std::vector<char> encodedData = encodeData(inputChars);
-//    std::string encodedString = std::string(encodedData.begin(), encodedData.end());
-////    std::cout << encodedString << std::endl;
-//    const std::vector<char> decodedData = decodeData(encodedData);
-//    std::string decodedString = std::string(decodedData.begin(), decodedData.end());
-////    std::cout << decodedString << std::endl;
+    //    delta codeing
+    //    const std::vector<char> encodedData = encodeData(inputChars);
+    //    std::string encodedString = std::string(encodedData.begin(), encodedData.end());
+    ////    std::cout << encodedString << std::endl;
+    //    const std::vector<char> decodedData = decodeData(encodedData);
+    //    std::string decodedString = std::string(decodedData.begin(), decodedData.end());
+    ////    std::cout << decodedString << std::endl;
     std::cout << std::endl;
     Node *test = creatingTree(creatyNodeList(counter(inputString)));
     createTable(test);
     std::string bitString = printedTable(inputString);
     std::vector<char> testing = bitWriting(bitString);
     wriringOnFile(testing,outputFileName);
-//    std::string bitStrint = printedTable(inputString);
-//    std::cout <<"This string of bits: "<< bitStrint;
+    //    std::string bitStrint = printedTable(inputString);
+    //    std::cout <<"This string of bits: "<< bitStrint;
     
-	return 0;
+    return 0;
 }
+
